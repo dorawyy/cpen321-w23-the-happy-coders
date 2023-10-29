de9ab50589105222568069139fcf3ad81c790c4b
Daniel Johnson <danieljohnson@Daniels-MBP.lan>
2023-10-29 13:46:12 -0700

    Changed to REST conventions

--- back-end/services/communicationService.js
+++ back-end/services/communicationService.js
@@ -10,7 +10,7 @@ async function sendMessage(chatroomId, content, sourceUserId){
     return true;
 }
 
-
+// Get all chatrooms associated with a user
 async function getChatrooms(userId){
     const user = await User.findById(userId)
     const chatroomIds = user.chatroomIds
@@ -25,11 +25,24 @@ async function getChatrooms(userId){
     return chatrooms
 }
 
+// Create a new chatroom
 async function createChatroom(user1Id, user2Id){
     const user1 = await User.findById(user1Id)
     const user2 = await User.findById(user2Id)
 
-    Chatroom.create()
+    const chatroom = await Chatroom.create({
+        messages: [],
+        user1Id: user1Id,
+        user2Id: user2Id
+    })
+
+    user1.chatroomIDs.push(chatroom._id)
+    user2.chatroomIDs.push(chatroom._id)
+
+    await user1.save()
+    await user2.save()
+
+    return chatroom._id
 }
 
 module.exports = {sendMessage, getChatrooms, createChatroom};
