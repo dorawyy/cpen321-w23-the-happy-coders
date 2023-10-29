be144ebdd4fdcc6ddb2745d63238f36fbf66e8ec
Daniel Johnson <danieljohnson@Daniels-MBP.lan>
2023-10-29 15:58:38 -0700

    changed format of endpoint for post to communication controller

--- back-end/routes/communicationRoutes.js
+++ back-end/routes/communicationRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const communicationController = require('../controllers/communicationController'); 
 
-router.get('/:id', communicationController.getChatrooms); 
-router.post('', communicationController.sendMessage); 
+router.get('/:userId', communicationController.getChatrooms); 
+router.post('/:id/messages', communicationController.sendMessage); 
 
 module.exports = router;
