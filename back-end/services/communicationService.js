94c23f8bc407eabef1c95b728d433fe63abc2d23
Daniel Johnson <danieljohnson@Daniels-MBP.lan>
2023-10-31 17:25:49 -0700

    open ai learning sessions

--- back-end/services/communicationService.js
+++ back-end/services/communicationService.js
@@ -1,11 +1,21 @@
 const { Chatroom } = require("../models/chatroom");
 const { User } = require("../models/user");
+const {OpenAIAPI} = require("openai")
+
 
 // Add message to database
-async function sendMessage(chatroomId, content, sourceUserId){
+async function sendMessage(chatroomId, content, sourceUserId, learningSession){
     const chatroom = await Chatroom.findById(chatroomId);
     chatroom.messages.push({sourceUserId, content})
+    
+    if(learningSession){
+        let openAIResponse = openAIMessage(content)
+        let id = "Assistant"
+        chatroom.messages.push({id,openAIResponse})
+    }
+
     await chatroom.save();
+
     return true;
 }
 
@@ -31,6 +41,13 @@ async function getChatrooms(userId){
     return chatrooms
 }
 
+// Get all chatrooms associated with a user
+async function getMessages(chatroomId){
+    const chatroom = await Chatroom.findById(chatroomId)
+
+    return chatroom.messages;
+}
+
 // Create a new chatroom
 async function createChatroom(user1Id, user2Id){
     const user1 = await User.findById(user1Id)
@@ -51,4 +68,50 @@ async function createChatroom(user1Id, user2Id){
     return chatroom._id
 }
 
-module.exports = {sendMessage, getChatrooms, createChatroom};
+// ChatGPT: Partial
+async function startLearningSession(){
+    const openai = new OpenAIAPI({ key: process.env.OPENAI});
+    const completion = await openai.chat.completions.create({
+        messages: [
+            { role: "system", content: `You are a helpful assistant that helps people learn languages. Your goal is to provide speaking prompts for a given proficiency level in a given language. The only text in your output should be in this format ["" , "" ...] The list should be of length 5.`},
+            { role: "user", content: "Provide me with a list of talking points" },
+        ],
+        model: "gpt-3.5-turbo",
+    });
+
+    try {
+        const jsonArray = JSON.parse(completion.choices[0]);
+        return jsonArray;
+      } catch (error) {
+        console.error('Error parsing JSON:', error);
+      }
+    return [];
+}
+
+// ChatGPT: Partial
+async function openAIMessage(message) {
+    const openai = new OpenAIAPI({ key: process.env.OPENAI});
+    const completion = await openai.chat.completions.create({
+        messages: [
+            { role: "system", content: "You are a helpful assistant that helps people learn languages. Your goal is to correct grammar mistakes and provide helpful tips. If the structure is correct, you should say always say 'No comment'" },
+            { role: "user", content: "I like you to." },
+            { role: "assistant", content: "The correct sentence structure in this sentence is 'I like you too'." },
+            { role: "user", content: "I like you too." },
+            { role: "assistant", content: "No comment." },
+            { role: "user", content: "She don't like ice cream." },
+            { role: "assistant", content: "The correct sentence structure is 'She doesn't like ice cream'." },
+            { role: "user", content: "There are a lot of people here." },
+            { role: "assistant", content: "No comment." },
+            { role: "user", content: "He go to school everyday." },
+            { role: "assistant", content: "The correct sentence structure is 'He goes to school every day'." },
+            { role: "user", content: "If I was there, I would have helped." },
+            { role: "assistant", content: "The correct sentence structure is 'If I were there, I would have helped'." },
+            { role: "user", content: message },
+    ],
+        model: "gpt-3.5-turbo",
+    });
+
+    return completion.choices[0];
+}
+
+module.exports = {sendMessage, getChatrooms, createChatroom, getMessages, openAIMessage, startLearningSession};
