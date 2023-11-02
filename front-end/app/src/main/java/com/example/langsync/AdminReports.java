bfe3e7432401a0b5b72968dbbd053822f3dbe9d2
Hasan Embabi <hembabi02@hotmail.com>
2023-11-01 17:50:17 -0700

    Added view for reports

---
+++ front-end/app/src/main/java/com/example/langsync/AdminReports.java
@@ -0,0 +1,14 @@
+package com.example.langsync;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminReports extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_reports);
+    }
+}
