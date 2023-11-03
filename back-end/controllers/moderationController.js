8cc3e03580f1844bc6ad0b6e61180deb27cb46e1
Hasan Embabi <hembabi02@hotmail.com>
2023-11-03 01:59:44 -0700

    Added return response for ban

--- back-end/controllers/moderationController.js
+++ back-end/controllers/moderationController.js
@@ -53,7 +53,8 @@ exports.banUser = async (req, res) => {
         const adminId = req.params.adminId;
         if (moderationService.isAdmin(adminId)) {
             const userId = req.body.userId;
-            return await moderationService.ban(userId);
+            await moderationService.ban(userId);
+            return res.status(200).json({ success: true });
         } else {
             return res.status(403).json({ success: false, message: "Unauthorized access to admin actions" })
         }
