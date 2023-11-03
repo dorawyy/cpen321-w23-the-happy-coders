63ea859dfc80d4bdd637c30e8a7e8fbd4cc118c2
Daniel Johnson <danieljohnson@Daniels-MBP.lan>
2023-11-02 20:57:38 -0700

    error logging

--- back-end/services/moderationService.js
+++ back-end/services/moderationService.js
@@ -6,47 +6,55 @@ const userService = require('./userService');
 //ChatGPT Usage: No
 // Adds a new report
 async function addReport(reportData){
-    const reporterUserId = reportData.reporterUserId;
-    const reportedUserId = reportData.reportedUserId;
-    const chatRoomId = reportData.chatRoomId;
-    const reportMessage = reportData.Report;
 
-    const reporterUser = await User.findById(reportData.reporterdUserId);
-    const reportedUser = await User.findById(reportData.reportedUserId);
-
-    const report = new Report({
-        reporterUserId: reporterUserId,
-        reportedUserId: reportedUserId,
-        chatRoomId: chatRoomId,
-        reportMessage: reportMessage
-    });
-
-    reporterUser.blockedUsers.push(reportData.reportedUserId);
-
-    reporterUser.matchedUsers = reporterUser.matchedUsers.filter(matchedUserId => matchedUserId != reportedUserId);
-    reporterUser.likedUsers = reporterUser.likedUsers.filter(likedUserId => likedUserId != reportedUserId);
-    reporterUser.chatroomIDs = reporterUser.chatroomIDs.filter(chatroomId => chatroomId != chatRoomId);
-
-    console.log("Reporter User")
-    console.log("Blocked users: " + reporterUser.blockedUsers);
-    console.log("Matched users: " + reporterUser.matchedUsers);
-    console.log("Liked users: " + reporterUser.likedUsers);
-    console.log("Chatroom IDs: " + reporterUser.chatroomIDs);
-
-    reportedUser.matchedUsers = reportedUser.matchedUsers.filter(matchedUserId => matchedUserId != reporterUserId);
-    reportedUser.likedUsers = reportedUser.likedUsers.filter(likedUserId => likedUserId != reporterUserId);
-    reportedUser.chatroomIDs = reportedUser.chatroomIDs.filter(chatroomId => chatroomId != chatRoomId);
-
-    console.log("Reported User")
-    console.log("Matched users: " + reportedUser.matchedUsers);
-    console.log("Liked users: " + reportedUser.likedUsers);
-    console.log("Chatroom IDs: " + reportedUser.chatroomIDs);
+    try{
+        console.log("here")
+        const reporterUserId = reportData.reporterUserId;
+        const reportedUserId = reportData.reportedUserId;
+        const chatRoomId = reportData.chatRoomId;
+        const reportMessage = reportData.Report;
     
-    await report.save();
-    await reportedUser.save();
-    await reporterUser.save();
-
-    return report;
+        const reporterUser = await User.findById(reportData.reporterdUserId);
+        const reportedUser = await User.findById(reportData.reportedUserId);
+    
+        const report = new Report({
+            reporterUserId: reporterUserId,
+            reportedUserId: reportedUserId,
+            chatRoomId: chatRoomId,
+            reportMessage: reportMessage
+        });
+    
+        reporterUser.blockedUsers.push(reportData.reportedUserId);
+    
+        reporterUser.matchedUsers = reporterUser.matchedUsers.filter(matchedUserId => matchedUserId != reportedUserId);
+        reporterUser.likedUsers = reporterUser.likedUsers.filter(likedUserId => likedUserId != reportedUserId);
+        reporterUser.chatroomIDs = reporterUser.chatroomIDs.filter(chatroomId => chatroomId != chatRoomId);
+    
+        console.log("Reporter User")
+        console.log("Blocked users: " + reporterUser.blockedUsers);
+        console.log("Matched users: " + reporterUser.matchedUsers);
+        console.log("Liked users: " + reporterUser.likedUsers);
+        console.log("Chatroom IDs: " + reporterUser.chatroomIDs);
+    
+        reportedUser.matchedUsers = reportedUser.matchedUsers.filter(matchedUserId => matchedUserId != reporterUserId);
+        reportedUser.likedUsers = reportedUser.likedUsers.filter(likedUserId => likedUserId != reporterUserId);
+        reportedUser.chatroomIDs = reportedUser.chatroomIDs.filter(chatroomId => chatroomId != chatRoomId);
+    
+        console.log("Reported User")
+        console.log("Matched users: " + reportedUser.matchedUsers);
+        console.log("Liked users: " + reportedUser.likedUsers);
+        console.log("Chatroom IDs: " + reportedUser.chatroomIDs);
+        
+        await report.save();
+        await reportedUser.save();
+        await reporterUser.save();
+    
+        return report;
+    }
+    catch(error){
+        console.log(error)
+    }
+   
 }
 
 
