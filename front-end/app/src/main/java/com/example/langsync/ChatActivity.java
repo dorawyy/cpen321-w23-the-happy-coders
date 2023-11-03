082455e922bcc540b7bb4710a36ee391d24498df
Leonardo Kamino Barros <leo.kamino@gmail.com>
2023-11-02 22:39:59 -0700

    Add new https server string

--- front-end/app/src/main/java/com/example/langsync/ChatActivity.java
+++ front-end/app/src/main/java/com/example/langsync/ChatActivity.java
@@ -67,7 +67,7 @@ public class ChatActivity extends AppCompatActivity {
 
     {
         try {
-            socket = IO.socket("http://4.204.191.217:443");
+            socket = IO.socket("https://langsyncapp.canadacentral.cloudapp.azure.com");
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
