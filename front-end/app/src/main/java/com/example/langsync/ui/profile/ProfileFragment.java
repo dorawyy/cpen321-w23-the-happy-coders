92b74a811c623ea1dd0b9e643af4c8e5b390c12b
Hasan Embabi <hembabi02@hotmail.com>
2023-11-02 23:00:53 -0700

    Implemented viewing the profile

--- front-end/app/src/main/java/com/example/langsync/ui/profile/ProfileFragment.java
+++ front-end/app/src/main/java/com/example/langsync/ui/profile/ProfileFragment.java
@@ -1,22 +1,46 @@
 package com.example.langsync.ui.profile;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.langsync.R;
 import com.example.langsync.databinding.FragmentProfileBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
     private Button videoButton;
+    private String userId;
+    private TextView profileAge, profileInterestedLanguages, profileProficientLanguages, profileLearningPreference;
 
+    private static final String TAG = "ProfileFragment";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,8 +50,55 @@ public class ProfileFragment extends Fragment {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        profileAge = root.findViewById(R.id.profile_age);
+        profileInterestedLanguages = root.findViewById(R.id.profile_interested_languages);
+        profileProficientLanguages = root.findViewById(R.id.profile_proficient_languages);
+        profileLearningPreference = root.findViewById(R.id.profile_learning_preference);
 
+        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        userId = sharedPreferences.getString("loggedUserId", null); // null is the default value if the key is not found
 
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(getString(R.string.base_url) + "users/" + userId)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d(TAG, "Failed to get user info");
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if(response.isSuccessful()) {
+                    try {
+                        Log.d(TAG, "Im over here");
+                        JSONObject json = new JSONObject(response.body().string());
+                        JSONObject user = json.getJSONObject("user");
+                        JSONArray proficientLanguages = user.getJSONArray("proficientLanguages");
+                        JSONArray interestedLanguages = user.getJSONArray("interestedLanguages");
+                        String age = user.getString("age");
+                        String learningPreference = user.getString("learningPreference");
+                        Log.d(TAG, "User info: " + user);
+
+                        getActivity().runOnUiThread( new Runnable() {
+                            @Override
+                            public void run() {
+                                profileAge.setText(age);
+                                profileProficientLanguages.setText(proficientLanguages.toString());
+                                profileInterestedLanguages.setText(interestedLanguages.toString());
+                                profileLearningPreference.setText(learningPreference);
+                            }
+                        });
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Log.d(TAG, "Failed to get user information");
+                }
+            }
+        });
         return root;
     }
 
