3b476ea5b1441c70b949ae4dd2b2911641e377f8
Hasan Embabi <hembabi02@hotmail.com>
2023-11-22 22:15:04 -0800

    Added test for non-functional requirement 2

--- front-end/app/src/test/java/com/example/langsync/ui/matches/GetRecommendationsTest.java
+++ front-end/app/src/test/java/com/example/langsync/ui/matches/GetRecommendationsTest.java
@@ -1,16 +1,16 @@
 package com.example.langsync.ui.matches;
 
 import org.junit.Test;
+
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-
-import com.example.langsync.R;
 
 import junit.framework.TestCase;
 
